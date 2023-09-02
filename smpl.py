49b7ac90eb15c06ec965211b888a7488c1bdd9f6
Felix Zhang <felixfzhang@hotmail.com>
2023-09-01 20:57:20 -0400

    add capacity to replay AMASS data

--- smpl.py
+++ smpl.py
@@ -37,57 +37,133 @@ def load_smpl_data(file_path: str) -> tuple[np.array, np.array]:
 
 class Movement:
     """"""
-    def __init__(self, data_file: str) -> None:
+    def __init__(self, data_file: str, start: int = 0) -> None:
         # self.model = load_model(model_path)
         # self.data = load_smpl_data(data_path)
 
         self.pose, self.trans = load_smpl_data(data_file)
 
         self.init_pos = self.pose[0]
-        self.count = 0
+        self.count = start
         self.num_frames = self.pose.shape[0]
 
         self.end = self.count == self.num_frames - 1
 
-    def get_next_action(self):
+    def get_next_action(self) -> np.array:
         self.count += 1
         if self.count == self.num_frames - 1:
             self.end = True
 
-        return self.pose[self.count]
+        """
+        Pelvis [0]
+          |  L_Hip [1]
+          |    |  L_Knee [4]
+          |    |    |  L_Ankle [7]
+          |    |----|----|- L_Toe [10]
+          |  R_Hip [2]
+          |    |  R_Knee [5]
+          |    |    |  R_Ankle [8]
+          |    |----|----|- R_Toe [11]
+          |  Torso [3]
+          |    |  Spine [6]
+          |    |    |  Chest [9]
+          |    |    |    |  Neck [12]
+          |    |    |    |    |- Head [15]
+          |    |    |    |  L_Thorax [13]
+          |    |    |    |    |  L_Shoulder [16]
+          |    |    |    |    |    |  L_Elbow [18]
+          |    |    |    |    |    |    |  L_Wrist [20]
+          |    |    |    |    |----|----|----|- L_Hand [22]
+          |    |    |    |  R_Thorax [14]
+          |    |    |    |    |  R_Shoulder [17]
+          |    |    |    |    |    |  R_Elbow [19]
+          |    |    |    |    |    |    |  R_Wrist [21]
+          |----|----|----|----|----|----|----|- R_Hand [23]
+        """
+        quats = np.array(([]))
+
+        # parts = [1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12, 15, 13, 16, 18, 20, 22, 14, 17, 19, 21, 23    # includes hands
+        parts = [0, 1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12, 15, 13, 16, 18, 20, 14, 17, 19, 21]
+        for i in parts:
+            r = scipy.spatial.transform.Rotation.from_rotvec(self.pose[self.count][i]).as_quat()
+            quats = np.append(quats, np.append(r[3], r[:3]))
+
+        return self.trans[self.count], quats
+
+    def step(self, m: mj.MjModel, d: mj.MjData) -> None:
+        """
+        data.qpos breakdown
+              [:3] - (x, y, z) Cartesian coordinate of the pelvis
+            [3:99] - (w, x, y, z) rotational quaterion of each part relative to its parent
+        """
+        center, quats = move.get_next_action()
+        d.qpos[: 3] = center
+        d.qpos[3: 3 + len(quats)] = quats
+
+        mj.mj_step(m, d)
+
+        if self.end:
+            print('Reached of the movement. Resetting sequence.')
+            self.count = 0
 
 
 if __name__ == '__main__':
     model, data = load_model('models/xml/humanoid_mesh.xml')
-    move = Movement('data/ACCAD/Female1Running_c3d/C2 - Run to stand_poses.npz')
+    move = Movement('data/ACCAD/Female1Running_c3d/C5 - walk to run_poses.npz', start=0)
 
     try:
+        import time
         view = viewer.launch_passive(model, data)
+        debug = False
+
     except RuntimeError:
         view = None
+        debug = True
 
-    import time
-
-    # for i in range(24):
-    #     data.ctrl[i] = i + 1
-
-    t1 = np.copy(data.qpos)
+    if debug:
+        qvel_max, qacc_max, qacc_39 = [], [], []
 
     while not move.end:
-        act = move.get_next_action()
+        move.step(model, data)
 
-        # print(data.qpos)
-        # data.qpos[:] = act
+        if debug:
+            qvel_max.append(max(data.qvel))
+            qacc_max.append(max(data.qacc))
+            qacc_39.append(data.qacc[42])
 
-        """
-        data.qpos breakdown
-              [:3] - (x, y, z) Cartesian coordinate of the pelvis 
-            [3:99] - (w, x, y, z) rotational quaterion of each part relative to its parent 
-        """
-        data.qpos[:] = t1
-
-        mj.mj_step(model, data)
         if view:
             view.sync()
+            time.sleep(0.01)
 
-        time.sleep(0.01)
+    if view:
+        view.close()
+
+    if debug:
+        import matplotlib.pyplot as plt
+
+        # plt.figure()
+        # plt.title('Velocity')
+        # plt.plot(qvel_max)
+        # plt.xlabel(r'Time ($t$)')
+        # # plt.xlim(400, len(qvel_max))
+        # plt.ylabel(r'Velocity ($m/s$)')
+        # plt.ylim(0, 50)
+        # plt.show()
+
+        plt.figure()
+        plt.title('Max Acceleration')
+        plt.plot(qacc_max)
+        plt.xlabel(r'Time ($t$)')
+        # plt.xlim(400, len(qacc_max))
+        plt.ylabel(r'Acceleration ($m/s^2$)')
+        plt.ylim(0, 1000)
+        plt.show()
+
+        plt.figure()
+        plt.title('Acceleration (DOF 39)')
+        plt.plot(qacc_39)
+        plt.xlabel(r'Time ($t$)')
+        # plt.xlim(400, len(qacc_max))
+        plt.ylabel(r'Acceleration ($m/s^2$)')
+        plt.ylim(0, 1000)
+        plt.show()
