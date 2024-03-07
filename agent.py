941879f34206ee6847e794f5f405b297a836c745
Felix Zhang <felixfzhang@hotmail.com>
2024-03-07 12:26:25 -0500

    Update optimizer hyperparameters

--- agent.py
+++ agent.py
@@ -9,6 +9,14 @@ class ActorCriticPolicy(MultiInputActorCriticPolicy):
     def __init__(self, *args, **kwargs):
         self.log_std_dist = Uniform(torch.tensor([-0.01]), torch.tensor([0.01]))
         super(ActorCriticPolicy, self).__init__(*args, **kwargs,
-                                              net_arch={'vf': [1024, 512], 'pi': [1024, 512]},
-                                              activation_fn=nn.ReLU,
-                                              log_std_init=self.log_std_dist.sample().float())
+                                                net_arch={'vf': [1024, 512], 'pi': [1024, 512]},
+                                                activation_fn=nn.ReLU,
+                                                log_std_init=self.log_std_dist.sample().float())
+
+        # Manually update the optimizer to enable per-layer hyperparameters to be set.
+        self.optimizer = torch.optim.SGD([{'params': self.pi_features_extractor.parameters(),
+                                           'lr': 0.000002,
+                                           'weight_decay': 0.0005},
+                                          {'params': self.vf_features_extractor.parameters(),
+                                           'lr': 0.0005,
+                                           'weight_decay': 0.}], momentum=0.9)
